d3b14f95409cc1af4eb493556a8bcfa7fb86572f
neo <ocalnuh@gmail.com>
2021-06-26 13:04:25 +0200

    second commit

--- jsCheat.md
+++ jsCheat.md
@@ -109,3 +109,16 @@
     var array = ['a', 'b', 'c'];
     console.log(...array);
 ```
+
+## Filter an Array
+
+#### The filter() method creates a new array with all elements that pass the test implemented by the provided function.
+
+```js
+    const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+
+    const result = words.filter(word => word.length > 6);
+
+    console.log(result);
+    // expected output: Array ["exuberant", "destruction", "present"]
+```
--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 
 let data = "";
-fs.readFile('./input1.txt', 'utf8', (error, r) => {
+fs.readFile('./Exercice-5/sample-5/input3.txt', 'utf8', (error, r) => {
     if (error) {
         console.error(error);
         return;
@@ -19,66 +19,129 @@ function main() {
     line.push(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
     let totalSum = line.reduce((a, b) => a + b);
 
-    let protectSum = 0;
-    let bag = initBag(AC);
+    let bag = initBag();
 
+    // Second Algo
     for (let i = 0; i < N; i++) {
-        let currentIndex = i;
-        let currentSum = calcSum(line, A, currentIndex, bag);
-        // console.log(i, currentSum);
-        if (superiorToNexts(line, A, currentIndex, C, currentSum, bag)) {
-            protectSum += currentSum;
-            i += A + C;
-        } else {
-            if (currentIndex >= AC) {
-                bag = manageBag(bag, AC, line[currentIndex - AC]);
+        bag = cleanBag(bag, i);
+        let nb = calcSum(line, A, i) + getMaxFromBag(bag, i);
+        bag = addToBag(bag, nb, i + AC - 1);
+    }
+
+    let result = totalSum - getMaxFromBag(bag, N + AC);
+
+    console.log(result);
+
+    // First Algo
+    // let protectSum = 0;
+    // let bag = initBagFirstAlgo(AC);
+
+    // for (let i = 0; i < N; i++) {
+    //     let currentIndex = i;
+    //     let currentSum = calcSum(line, A, currentIndex, bag);
+    //     // console.log(i, currentSum);
+    //     if (superiorToNexts(line, A, currentIndex, C, currentSum, bag)) {
+    //         protectSum += currentSum;
+    //         i += A + C;
+    //     } else {
+    //         if (currentIndex >= AC) {
+    //             bag = manageBag(bag, AC, line[currentIndex - AC]);
+    //         }
+    //     }
+    //     // console.log(i, protectSum);
+    // }
+    // // console.log(totalSum - 249);
+}
+
+// function superiorToNexts(line, A, currentIndex, C, currentSum, bag) {
+//     let b = bag.slice();
+//     currentIndex++;
+//     for (let i = 0; i < (A + C); i++) {
+//         if (currentIndex >= (A + C)) {
+//             b = manageBag(b, (A + C), line[currentIndex - (A + C)]);
+//         }
+//         if (currentSum < (calcSum(line, A, currentIndex + i, b))) {
+//             return false;
+//         }
+//     }
+//     return true;
+// }
+
+// function calcSum(line, A, currentIndex, bag) {
+//     let sum = 0 + Math.max(...bag);
+//     for (let i = currentIndex; i < A + currentIndex; i++) {
+//         sum += isNaN(line[i])?0:line[i];
+//     }
+
+//     // console.log("debug", currentIndex + "/" + (currentIndex + A + 7) + " - " + sum);
+//     // console.log(line.slice(currentIndex, A + currentIndex).join(''));
+
+//     return sum;
+// }
+
+// function initBagFirstAlgo(AC) {
+//     let bag = [];
+//     for (let i = 0; i < AC; i++) {
+//         bag.push(0);
+//     }
+//     return bag;
+// }
+
+// function manageBag(bag, AC, enter) {
+//     let input = bag.shift() + enter;
+//     if (input > Math.max(...bag)) {
+//         bag.push(input);
+//     } else {
+//         bag.push(0);
+//     }
+//     return bag;
+// }
+
+function initBag() {
+    let bag = [{nb:0, ind:0}];
+    return bag;
+}
+
+function cleanBag(bag, index) {
+    maxIndex = -1;
+    for (let i = 0; i < bag.length; i++) {
+        if (bag[i].ind >= index) {
+            break;
+        }
+        maxIndex = bag[i].ind;
+    }
+    bag = bag.filter(e => e.ind >= maxIndex);
+    return bag;
+}
+
+function addToBag(bag, number, index) {
+    let bagMaxNumber = 0;
+    for (let i = 0; i < bag.length; i++) {
+        if (bag[i].nb > bagMaxNumber) {
+            bagMaxNumber = bag[i].nb;
+            if (bagMaxNumber >= number) {
+                return bag;
             }
         }
-        // console.log(i, protectSum);
     }
-    // console.log(totalSum - 249);
+    bag.push({nb: number, ind: index});
+    return bag;
 }
 
-function superiorToNexts(line, A, currentIndex, C, currentSum, bag) {
-    let b = bag.slice();
-    currentIndex++;
-    for (let i = 0; i < (A + C); i++) {
-        if (currentIndex >= (A + C)) {
-            b = manageBag(b, (A + C), line[currentIndex - (A + C)]);
-        }
-        if (currentSum < (calcSum(line, A, currentIndex + i, b))) {
-            return false;
+function getMaxFromBag(bag, index) {
+    let bagMaxNumber = 0;
+    for (let i = 0; i < bag.length; i++) {
+        if (bag[i].nb > bagMaxNumber && bag[i].ind < index) {
+            bagMaxNumber = bag[i].nb;
         }
     }
-    return true;
+    return bagMaxNumber;
 }
 
-function calcSum(line, A, currentIndex, bag) {
-    let sum = 0 + Math.max(...bag);
+function calcSum(line, A, currentIndex) {
+    let sum = 0;
     for (let i = currentIndex; i < A + currentIndex; i++) {
         sum += isNaN(line[i])?0:line[i];
     }
-
-    // console.log("debug", currentIndex + "/" + (currentIndex + A + 7) + " - " + sum);
-    // console.log(line.slice(currentIndex, A + currentIndex).join(''));
-
     return sum;
-}
-
-function initBag(AC) {
-    let bag = [];
-    for (let i = 0; i < AC; i++) {
-        bag.push(0);
-    }
-    return bag;
-}
-
-function manageBag(bag, AC, enter) {
-    let input = bag.shift() + enter;
-    if (input > Math.max(...bag)) {
-        bag.push(input);
-    } else {
-        bag.push(0);
-    }
-    return bag;
 }
