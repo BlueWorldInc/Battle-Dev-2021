089b93325fbedd0f836dc4bb7126029e53d184b9
neo <ocalnuh@gmail.com>
2021-06-27 13:16:08 +0200

    save

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const {performance} = require('perf_hooks');
 
 let data = "";
 fs.readFile('./Exercice-5/sample-5/input3.txt', 'utf8', (error, r) => {
@@ -7,7 +8,12 @@ fs.readFile('./Exercice-5/sample-5/input3.txt', 'utf8', (error, r) => {
         return;
     }
     data = r;
-    main();
+    const t0 = performance.now();
+    for (let i = 0; i < 10; i++) {
+        main();
+    }
+    const t1 = performance.now();
+    console.log(`Call to doSomething took ${(t1 - t0).toFixed(2)} milliseconds.`);
 });
 
 function main() {
@@ -23,14 +29,16 @@ function main() {
 
     // Second Algo
     for (let i = 0; i < N; i++) {
+        let t0 = performance.now();
         bag = cleanBag(bag, i);
+        let t1 = performance.now();
         let nb = calcSum(line, A, i) + getMaxFromBag(bag, i);
         bag = addToBag(bag, nb, i + AC - 1);
     }
 
     let result = totalSum - getMaxFromBag(bag, N + AC);
 
-    console.log(result);
+    // console.log(result);
 
     // First Algo
     // let protectSum = 0;
@@ -145,3 +153,89 @@ function calcSum(line, A, currentIndex) {
     }
     return sum;
 }
+
+
+// /*******
+//  * Read input from STDIN
+//  * Use: console.log()  to output your result.
+//  * Use: console.error() to output debug information into STDERR
+//  * ***/
+
+//  var input = [];
+//  let bag = [{nb:0, ind:0}];
+ 
+//  readline_object.on("line", (value) => { //Read input values
+//      input.push(value);
+//  })
+//  //Call ContestResponse when all inputs are read
+//  readline_object.on("close", ContestResponse); 
+ 
+ 
+//  function ContestResponse() {
+//      main(input);
+//  }
+ 
+//  function main(data) {
+ 
+//      let N = parseInt(data[0].split(' ')[0]);
+//      let A = parseInt(data[0].split(' ')[1]);
+//      let C = parseInt(data[0].split(' ')[2]);
+//      let AC = A + C;
+//      let line = data[1].split(' ').map(e => parseInt(e));
+//      line.push(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
+//      let totalSum = line.reduce((a, b) => a + b);
+ 
+//      // Second Algo
+//      for (let i = 0; i < N; i++) {
+//          cleanBag(i);
+//          let nb = calcSum(line, A, i) + getMaxFromBag(i);
+//          addToBag(nb, i + AC - 1);
+//      }
+ 
+//      let result = totalSum - getMaxFromBag(N + AC);
+ 
+//      console.log(result);
+ 
+//  }
+ 
+//  function cleanBag(index) {
+//      maxIndex = -1;
+//      for (let i = 0; i < bag.length; i++) {
+//          if (bag[i].ind >= index) {
+//              break;
+//          }
+//          maxIndex = bag[i].ind;
+//      }
+//      bag = bag.filter(e => e.ind >= maxIndex);
+//  }
+ 
+//  function addToBag(number, index) {
+//      let bagMaxNumber = 0;
+//      for (let i = 0; i < bag.length; i++) {
+//          if (bag[i].nb > bagMaxNumber) {
+//              bagMaxNumber = bag[i].nb;
+//              if (bagMaxNumber >= number) {
+//                  return;
+//              }
+//          }
+//      }
+//      bag.push({nb: number, ind: index});
+//  }
+ 
+//  function getMaxFromBag(index) {
+//      let bagMaxNumber = 0;
+//      for (let i = 0; i < bag.length; i++) {
+//          if (bag[i].nb > bagMaxNumber && bag[i].ind < index) {
+//              bagMaxNumber = bag[i].nb;
+//          }
+//      }
+//      return bagMaxNumber;
+//  }
+ 
+//  function calcSum(line, A, currentIndex) {
+//      let sum = 0;
+//      for (let i = currentIndex; i < A + currentIndex; i++) {
+//          sum += line[i];
+//      }
+//      return sum;
+//  }
